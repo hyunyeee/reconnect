1fe04ba959c94a2dfa6cb1df238a1cb5930926e3
hyunyeee <5004ohy@naver.com>
2026-01-19 17:20:51 +0900

    chore: 카카오 애드핏 재설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import QueryProvider from "@/app/providers/QueryProvider";
 import { Providers } from "@/app/Providers";
 import OverlayRenderer from "@/components/overlay/OverlayRenderer";
 import GoogleAnalyticsPageView from "@/components/analytics/GoogleAnalyticsPageView";
+import { KakaoAdFit } from "@/components/KaKaoAdFit";
 
 export const metadata: Metadata = {
   title: "LoveReconnect",
@@ -28,10 +29,10 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html lang="ko" className={pretendard.variable}>
       <head>
         {/* Google AdSense */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9257573330331971"
-        />
+        {/*<script*/}
+        {/*  async*/}
+        {/*  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9257573330331971"*/}
+        {/*/>*/}
 
         {/* GA */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-FD8Q76DJ1N" />
@@ -58,13 +59,13 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             {/* 메인 컨텐츠 */}
             <main className="mx-auto w-full max-w-md px-4 pt-6">{children}</main>
 
-            {/*<footer className="mt-16 border-t border-gray-100 py-8">*/}
-            {/*  <div className="mx-auto flex max-w-md flex-col items-center gap-6 px-4">*/}
-            {/*    <KakaoAdFit unit="DAN-aLlyYJ68qqHuPvdY" width={300} height={250} />*/}
-            {/*    <KakaoAdFit unit="DAN-ZOGkyfi9vPA93ivl" width={300} height={250} />*/}
-            {/*    <KakaoAdFit unit="DAN-wAz4OD2dxCo8DFqy" width={320} height={100} />*/}
-            {/*  </div>*/}
-            {/*</footer>*/}
+            <footer className="mt-16 border-t border-gray-100 py-8">
+              <div className="mx-auto flex max-w-md flex-col items-center gap-6 px-4">
+                <KakaoAdFit unit="DAN-aLlyYJ68qqHuPvdY" width={300} height={250} />
+                <KakaoAdFit unit="DAN-ZOGkyfi9vPA93ivl" width={300} height={250} />
+                <KakaoAdFit unit="DAN-wAz4OD2dxCo8DFqy" width={320} height={100} />
+              </div>
+            </footer>
 
             <Toaster position="top-center" richColors closeButton />
           </QueryProvider>
